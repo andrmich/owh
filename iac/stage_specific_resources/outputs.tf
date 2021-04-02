c2182142dcb82337659d6fc8049779ac934aeda4
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2021-04-02 14:48:18 +0200

    stage resources output changes

--- iac/stage_specific_resources/outputs.tf
+++ iac/stage_specific_resources/outputs.tf
@@ -6,3 +6,14 @@ output "access_from_bastion_sg_id" {
   description = "Security group ID that allows access to the resources from bastion."
   value       = concat(module.access_from_bastion_sg.*.this_security_group_id, [""])[0]
 }
+
+output "alb_sg_id" {
+  value = aws_security_group.alb_sg.id
+}
+output "alb_dns_name" {
+  value = aws_alb.simple_vm.dns_name
+}
+
+output "alb_secure_listener_arn" {
+  value = aws_alb_listener.secure_http.arn
+}
