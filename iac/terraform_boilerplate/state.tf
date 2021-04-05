f452ace52e98a0df16dd24d03d8448d72ad1eb98
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2021-04-05 13:01:21 +0200

    add comments to state.tf

--- iac/terraform_boilerplate/state.tf
+++ iac/terraform_boilerplate/state.tf
@@ -8,10 +8,14 @@ terraform {
     }
   }
 
+  # Before running terraform init for the first time check README.md for instructions.
+  ####### COMMENT BETWEEN HERE AND LINE 19 #######
   backend "s3" {
     bucket         = "aisim-tf-state"
     key            = "terraform-boilerplate"
     region         = "eu-central-1" # Bucket region, not deployment region
     dynamodb_table = "terraform-state-locking"
   }
+  ####### COMMENT BETWEEN HERE AND LINE 12 #######
+
 }
